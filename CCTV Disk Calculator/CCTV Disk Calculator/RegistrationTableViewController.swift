2a06e057462ebf8a2be7fcd1df2cfe17b77c5359
joshersratters <joshratcliffe@me.com>
2015-05-03 18:23:26 +0100

    Add textfield keyboard navigation

--- CCTV Disk Calculator/CCTV Disk Calculator/RegistrationTableViewController.swift
+++ CCTV Disk Calculator/CCTV Disk Calculator/RegistrationTableViewController.swift
@@ -8,13 +8,23 @@
 
 import UIKit
 
-class RegistrationTableViewController: UITableViewController {
+class RegistrationTableViewController: UITableViewController, UITextFieldDelegate {
     @IBOutlet weak var firstNameTextField: UITextField!
     @IBOutlet weak var lastNameTextField: UITextField!
     @IBOutlet weak var emailTextField: UITextField!
+    
+    struct formValues {
+        var firstName: String = RegistrationTableViewController().firstNameTextField.description
+        var lastName: String = RegistrationTableViewController().lastNameTextField.description
+        var email: String = RegistrationTableViewController().emailTextField.description
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.firstNameTextField.delegate = self
+        self.lastNameTextField.delegate = self
+        self.emailTextField.delegate = self
         self.navigationItem.backBarButtonItem?.tintColor = UIColor.whiteColor()
     }
 
@@ -22,5 +32,22 @@ class RegistrationTableViewController: UITableViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        
+        switch textField {
+        case firstNameTextField:
+            textField.resignFirstResponder()
+            lastNameTextField.becomeFirstResponder()
+        case lastNameTextField:
+            textField.resignFirstResponder()
+            emailTextField.becomeFirstResponder()
+        case emailTextField:
+            emailTextField.resignFirstResponder()
+        default:
+            true
+        }
+        
+        return true
+    }
 }
